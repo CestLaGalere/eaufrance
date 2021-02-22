48840660d3061ba34ac87f427f0f40bd9fd05b38
chris.galley <chris.galley@entelis.com>
2021-02-22 11:24:16 +0100

    commented

--- custom_components/eaufrance/sensor.py
+++ custom_components/eaufrance/sensor.py
@@ -123,6 +123,7 @@ class EauFranceData():
         self._device_class = device_class
         self._time_zone = hass.config.time_zone
         self.data = None
+
         if device_class == "H":
             self.unit = "m"
         else:
@@ -144,7 +145,9 @@ class EauFranceData():
                 return
 
             self.data = obs["resultat_obs"] / 1000
-            if self.data < 10:
+
+            # show under 10m in cm, otherwise to 1 dp.
+            if self.data < 10.0:
                 self.data = round(self.data, 2)
             else:
                 self.data = round(self.data, 1)
@@ -169,6 +172,7 @@ class EauFranceData():
         # example https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16
 
         base_url="https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr"
+        # sort so the most recent is first - only need to read the first result
         params = {
             "code_entite" : self._device_id,
             "grandeur_hydro": self._device_class,
@@ -178,7 +182,7 @@ class EauFranceData():
 
         now = datetime.now()
         now += self._time_zone.utcoffset(now)
-        start_of_period = now - timedelta(hours = 4)
+        start_of_period = now - timedelta(hours = 4)    # get 4 hours readings
         params.update({"date_debut_obs" : start_of_period.strftime("%Y-%m-%dT%H:%M:%S")})
 
         all_params = '&'.join('{0}={1}'.format(key, val) for key, val in params.items())
@@ -187,19 +191,57 @@ class EauFranceData():
 
 
     def get_results_data(self):
+        """
+        Return the array of readings
+        """
         url = self.get_device_history_url()
         response = requests.get(url)
         if response.status_code != requests.codes.ok:
             raise Exception("requests getting data: {0}".format(response.status_code))
         content = response.content.decode()
+        # returned example (split to ned lines for readability - no line breaks in reality)
+        # after the date_obs and resultat_obs values
         #content = '{"count":10,"first":"https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16&page=1&size=10","prev":null,"next":null,"api_version":"1.0.1","data":[{"code_site":"O5882510","code_station":"O588251001","grandeur_hydro":"Q","date_debut_serie":"2021-02-16T00:00:00Z","date_fin_serie":"2021-02-16T10:00:00Z","statut_serie":4,"code_systeme_alti_serie":31,"date_obs":"2021-02-16T10:00:00Z","resultat_obs":147629.0,"code_methode_obs":12,"libelle_methode_obs":"Interpolation","code_qualification_obs":16,"libelle_qualification_obs":"Non qualifiée","continuite_obs_hydro":true,"longitude":1.340482605,"latitude":44.091553707}]}'
+        #content = '{
+        # "count":10,
+        # "first":"https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16&page=1&size=10",
+        # "prev":null,
+        # "next":null,
+        # "api_version":"1.0.1",
+        # "data":[
+        #   {"code_site":"O5882510",
+        #   "code_station":"O588251001",
+        #   "grandeur_hydro":"Q",
+        #   "date_debut_serie":"2021-02-16T00:00:00Z",
+        #   "date_fin_serie":"2021-02-16T10:00:00Z",
+        #   "statut_serie":4,"code_systeme_alti_serie":31,
+        #   "date_obs":"2021-02-16T10:00:00Z",
+        #   "resultat_obs":147629.0,
+        #   "code_methode_obs":12,
+        #   "libelle_methode_obs":"Interpolation",
+        #   "code_qualification_obs":16,
+        #   "libelle_qualification_obs":"Non qualifiée",
+        #   "continuite_obs_hydro":true,
+        #   "longitude":1.340482605,
+        #   "latitude":44.091553707
+        #   }
+        # <more data>
+        # ]}'
+
+        # not true python dictionary so need to reformat slightly
         content = content.replace(":null", ":None")
         content = content.replace(":true", ":True")
+
         root = ast.literal_eval(content)
         d = root["data"]
         return d
 
     def get_first_reading(self):
+        """
+        Extract data_obs and resultat_obs from the first reading as this is the most up to date reading available
+        return
+        dictionary with these two key/values
+        """
         d = self.get_results_data()
         reading = {"date_obs": d[0]["date_obs"], "resultat_obs": d[0]["resultat_obs"]}
         return reading
