68c55b77339eb7f6d020076466512c675d35b043
chris.galley <chris.galley@entelis.com>
2021-03-29 20:28:54 +0200

    uses utc correctly

--- custom_components/eaufrance/sensor.py
+++ custom_components/eaufrance/sensor.py
@@ -192,14 +192,14 @@ class EauFranceData():
             "sort": "desc"
             }
 
-        now = datetime.now()
+        #now = datetime.now()
         now_utc = dt_util.utcnow()
-        start_of_period = now_utc - timedelta(hours = 4)    # get 4 hours readings
+        start_of_period = now_utc - timedelta(hours = 2)    # get 4 hours readings
         params.update({"date_debut_obs" : start_of_period.strftime("%Y-%m-%dT%H:%M:%S")})
 
-        _LOGGER.warning("now          : {0}".format(now.strftime("%Y-%m-%dT%H:%M:%S")))
-        _LOGGER.warning("utc          : {0}".format(now_utc.strftime("%Y-%m-%dT%H:%M:%S")))
-        _LOGGER.warning("startofperiod: {0}".format(start_of_period.strftime("%Y-%m-%dT%H:%M:%S")))
+        #_LOGGER.warning("now          : {0}".format(now.strftime("%Y-%m-%dT%H:%M:%S")))
+        #_LOGGER.warning("utc          : {0}".format(now_utc.strftime("%Y-%m-%dT%H:%M:%S")))
+        #_LOGGER.warning("startofperiod: {0}".format(start_of_period.strftime("%Y-%m-%dT%H:%M:%S")))
 
         all_params = '&'.join('{0}={1}'.format(key, val) for key, val in params.items())
         
