8e6d0619054408ab6aaeb2922d1c221b13e47221
Chris Galley <chris.galley@entelis.com>
2022-01-14 13:06:33 +0100

    replace device_state_attributes added type declarations

--- custom_components/eaufrance/sensor.py
+++ custom_components/eaufrance/sensor.py
@@ -3,11 +3,10 @@ from datetime import datetime, timedelta, time
 import pytz
 import logging
 import ast
-#from typing import Any, Callable, Dict, Optional
+from typing import Any, Dict, Mapping, Optional
 
 import voluptuous as vol
 
-from homeassistant import config_entries, core
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
 from homeassistant.const import (
@@ -17,10 +16,13 @@ from homeassistant.const import (
     CONF_DEVICE_ID,
 )
 
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.entity import Entity
-#from homeassistant.helpers.typing import (
+# from homeassistant.helpers.typing import (
 #    ConfigType,
 #    DiscoveryInfoType,
 #    HomeAssistantType,
@@ -53,7 +55,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(hass: HomeAssistant, config: ConfigEntry, async_add_entities: AddEntitiesCallback, discovery_info=None) -> None:
     session = async_get_clientsession(hass)
     name = config.get(CONF_NAME)
     device_class = config.get(CONF_DEVICE_CLASS)
@@ -66,27 +68,35 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     )
 
 
+class EauFranceData():
+    pass
+
+
 class VigicruesSensor(Entity):
     """Implementation of an EauFrance sensor."""
 
-    def __init__(self, name, efd):
+    def __init__(self, name: str, efd: EauFranceData) -> None:
         """Initialize the sensor."""
         self._name = name
         self._efd = efd
         self._state = None
         self._unit_of_measurement = ""
         self._unique_id = efd.unique_id
+        self._attributes = {
+            "unique_id": self._unique_id,
+            ATTR_ATTRIBUTION: ATTRIBUTION.format("EauFrance"),
+        }
 
     @classmethod
-    def current(cls, name, efd):
+    def current(cls, name: str, efd: EauFranceData):
         return cls(name, efd)
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         return self._unique_id
 
     @property
@@ -94,27 +104,26 @@ class VigicruesSensor(Entity):
         return self._state
 
     @property
-    def unit_of_measurement(self):
+    def unit_of_measurement(self) -> str:
         return self._unit_of_measurement
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         if self._efd.device_class == "H":
             return "mdi:waves"
         return "mdi:fast-forward"
 
     @property
-    def device_state_attributes(self):
-        source = "EauFrance"
-        return {ATTR_ATTRIBUTION: ATTRIBUTION.format(source)}
+    def extra_state_attributes(self) -> Mapping[str, Any] | None:
+        """Return the entity specific state attributes."""
+        return self._attributes
 
-
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from EauFrance and updates the state."""
 
-        #try:
+        # try:
         self._efd.update(self.hass)
-        #except:
+        # except:
         #    _LOGGER.error("Exception when getting EauFrance web update data")
         #    return
 
@@ -127,7 +136,7 @@ class EauFranceData():
     device_class must be H or Q
     """
 
-    def __init__(self, hass, device_id, device_class):
+    def __init__(self, hass: HomeAssistant, device_id: str, device_class: str) -> None:
         self._device_id = device_id
         self._device_class = device_class
         self._time_zone = hass.config.time_zone
@@ -139,15 +148,15 @@ class EauFranceData():
             self.unit = "m³/s"
 
     @property
-    def device_class(self):
+    def device_class(self) -> str:
         return self._device_class
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         return "edf_{}_{}".format(self._device_id, self._device_class)
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self, hass):
+    def update(self, hass: HomeAssistant) -> None:
         # get readings from eurfrance website
         try:
             obs = self.get_first_reading()
@@ -173,10 +182,10 @@ class EauFranceData():
         except TimeoutError:
             _LOGGER.warning("Timeout connecting to EauFrance URL")
         except Exception as e:
-            _LOGGER.warning("{0} occurred in update. Details: {1}".format(e.__class__, e))
+            _LOGGER.warning(
+                "{0} occurred in update. Details: {1}".format(e.__class__, e))
 
-
-    def get_device_history_url(self):
+    def get_device_history_url(self) -> str:
         """
         Create url to get the last 4h of readings for this station
         Parameters
@@ -187,28 +196,30 @@ class EauFranceData():
 
         # example https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16
 
-        base_url="https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr"
+        base_url = "https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr"
         # sort so the most recent is first - only need to read the first result
         params = {
-            "code_entite" : self._device_id,
+            "code_entite": self._device_id,
             "grandeur_hydro": self._device_class,
             "timestep": "60",
             "sort": "desc"
-            }
+        }
 
         now_utc = dt_util.utcnow()
-        start_of_period = now_utc - timedelta(hours = 2)    # get 4 hours readings
-        params.update({"date_debut_obs" : start_of_period.strftime("%Y-%m-%dT%H:%M:%S")})
+        start_of_period = now_utc - \
+            timedelta(hours=2)    # get 4 hours readings
+        params.update(
+            {"date_debut_obs": start_of_period.strftime("%Y-%m-%dT%H:%M:%S")})
 
         #now = datetime.now()
         #_LOGGER.warning("now          : {0}".format(now.strftime("%Y-%m-%dT%H:%M:%S")))
         #_LOGGER.warning("utc          : {0}".format(now_utc.strftime("%Y-%m-%dT%H:%M:%S")))
         #_LOGGER.warning("startofperiod: {0}".format(start_of_period.strftime("%Y-%m-%dT%H:%M:%S")))
 
-        all_params = '&'.join('{0}={1}'.format(key, val) for key, val in params.items())
-        
-        return base_url + "?" + all_params
+        all_params = '&'.join('{0}={1}'.format(key, val)
+                              for key, val in params.items())
 
+        return base_url + "?" + all_params
 
     def get_results_data(self):
         """
@@ -217,12 +228,13 @@ class EauFranceData():
         url = self.get_device_history_url()
         response = requests.get(url)
         if response.status_code != requests.codes.ok:
-            raise Exception("requests: getting data: {0}\n{1}\n{2}".format(response.status_code, url, response.content))
+            raise Exception("requests: getting data: {0}\n{1}\n{2}".format(
+                response.status_code, url, response.content))
         content = response.content.decode()
         # returned example (split to ned lines for readability - no line breaks in reality)
         # after the date_obs and resultat_obs values
         #content = '{"count":10,"first":"https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16&page=1&size=10","prev":null,"next":null,"api_version":"1.0.1","data":[{"code_site":"O5882510","code_station":"O588251001","grandeur_hydro":"Q","date_debut_serie":"2021-02-16T00:00:00Z","date_fin_serie":"2021-02-16T10:00:00Z","statut_serie":4,"code_systeme_alti_serie":31,"date_obs":"2021-02-16T10:00:00Z","resultat_obs":147629.0,"code_methode_obs":12,"libelle_methode_obs":"Interpolation","code_qualification_obs":16,"libelle_qualification_obs":"Non qualifiée","continuite_obs_hydro":true,"longitude":1.340482605,"latitude":44.091553707}]}'
-        #content = '{
+        # content = '{
         # "count":10,
         # "first":"https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite=O588251001&grandeur_hydro=Q&timestep=60&sort=desc&date_debut_obs=2021-02-16&page=1&size=10",
         # "prev":null,
@@ -256,18 +268,19 @@ class EauFranceData():
         if "data" not in root:
             _LOGGER.warning(root)
             raise Exception("data not found in root")
-        
+
         count = root["count"]
         if count == 0:
-            raise Exception("No observations returned for {0}".format(self._device_id))
-            
+            raise Exception(
+                "No observations returned for {0}".format(self._device_id))
+
         d = root["data"]
         if len(d) == 0:
             _LOGGER.warning(root)
             raise Exception("data contains no readings")
         return d
 
-    def get_first_reading(self):
+    def get_first_reading(self) -> Dict[str, str]:
         """
         Extract data_obs and resultat_obs from the first reading as this is the most up to date reading available
         return
@@ -280,5 +293,6 @@ class EauFranceData():
             _LOGGER.warning(first_reading)
             raise Exception("unexpected format of first_reading")
 
-        reading = {"date_obs": first_reading["date_obs"], "resultat_obs": first_reading["resultat_obs"]}
+        reading = {"date_obs": first_reading["date_obs"],
+                   "resultat_obs": first_reading["resultat_obs"]}
         return reading
